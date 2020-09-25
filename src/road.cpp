3e3a2fd283174cde38d431854f898a6b300f39e5
Xiada <69246170+XiadaOku@users.noreply.github.com>
2020-09-25 18:05:52 +0500

    Update road.cpp

--- src/road.cpp
+++ src/road.cpp
@@ -2759,9 +2759,3 @@ void ShowImageKeyPress(SDL_Event *k)
 {
 	ShowImageKeyFlag = 1;
 }
-
-
-bool SetAchievement(std::string name) {
-	std::cout<<"Set achievement:"<<name<<std::endl;
-	return true;
-}
